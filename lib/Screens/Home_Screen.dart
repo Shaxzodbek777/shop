9c5e4d9d18d228fd9b59301f4e02f807901e962c
Shaxzodek777 <shaxzodbek206@gmail.com>
2024-03-17 14:41:57 +0300

    Initial commit

---
+++ lib/Screens/Home_Screen.dart
@@ -0,0 +1,86 @@
+import 'package:flutter/material.dart';
+import 'package:ionicons/ionicons.dart';
+
+import '../Models/product.dart';
+import '../Widgets/categories.dart';
+import '../Widgets/home_appbar.dart';
+import '../Widgets/home_slider.dart';
+import '../Widgets/product_card.dart';
+import '../Widgets/search_field.dart';
+import '../constants.dart';
+
+
+class HomeScreen extends StatefulWidget {
+  const HomeScreen({super.key});
+
+  @override
+  State<HomeScreen> createState() => _HomeScreenState();
+}
+
+class _HomeScreenState extends State<HomeScreen> {
+  int currentSlide = 0;
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      backgroundColor: kscaffoldColor,
+      body: SafeArea(
+        child: SingleChildScrollView(
+          child: Padding(
+            padding: const EdgeInsets.all(10),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: [
+                const HomeAppBar(),
+                const SizedBox(height: 20),
+                const SearchField(),
+                const SizedBox(height: 20),
+                HomeSlider(
+                  onChange: (value) {
+                    setState(() {
+                      currentSlide = value;
+                    });
+                  },
+                  currentSlide: currentSlide,
+                ),
+                const SizedBox(height: 20),
+                const Categories(),
+                const SizedBox(height: 25),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: [
+                    const Text(
+                      "Special For You",
+                      style: TextStyle(
+                        fontSize: 24,
+                        fontWeight: FontWeight.bold,
+                      ),
+                    ),
+                    TextButton(
+                      onPressed: () {},
+                      child: const Text("See all"),
+                    ),
+                  ],
+                ),
+                const SizedBox(height: 10),
+                GridView.builder(
+                  physics: const NeverScrollableScrollPhysics(),
+                  shrinkWrap: true,
+                  gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                    crossAxisCount: 2,
+                    crossAxisSpacing: 20,
+                    mainAxisSpacing: 20,
+                  ),
+                  itemCount: products.length,
+                  itemBuilder: (context, index) {
+                    return ProductCard(product: products[index]);
+                  },
+                ),
+              ],
+            ),
+          ),
+        ),
+      ),
+    );
+  }
+}
